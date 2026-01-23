4f447dcc133ca37dbbcb034c0bdf0ed7c30e6ca1
IdanYeh <idanyehuda17@gmail.com>
2026-01-23 13:54:13 +0200

    Implementing Unet

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def main(name):
+    nn.Conv2d
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
